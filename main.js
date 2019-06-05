b182d5dbbc556fa1f8d8ac70866a9c2dd6be1e50
Huy Hoang <huyhoang0496@gmail.com>
2019-06-05 12:40:04 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+//const {app, BrowserWindow} = require('electron')
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 1280, height: 720 });
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
